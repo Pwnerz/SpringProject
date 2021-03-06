63b2ee053723197e08c3528d98f1654ff79804b9
Alex <supamumas@gmail.com>
2021-03-06 16:21:49 +0100

    First commit.

---
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,57 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
+//Controller
+@Controller
+//Adds an app to the route. To get to index you have to go through it. /app/index || home . http://localhost:8080/app/index
+@RequestMapping("/app")
+public class IndexController {
+	
+	//Mappeador que carga una ruta/vista mediante url. Puede ser GetMapping, PostMapping...
+	@RequestMapping({"/index", "/","","/home"})
+	//Adding stuff to the model.
+	// Sustituible por Map<String,Object> map || Abajo seria map.put();
+	//Otra manera mas es ModelAndView mv || mv.addObject(); || mv.setViewName("index");||Se cambia a return mv;
+	public String index(Model model) {
+		//titulo del html se muestra a traves del atributo titulo.
+		model.addAttribute("titulo", "Hola Spring Framework con Model!");
+		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Pepe"); 
+		usuario.setApellido("Perez");
+		usuario.setEmail("pepe@gmail.com");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		
+		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+	model.addAttribute("titulo", "Listado de usuarios");
+	return "/listar";
+	}
+	//Usamos el ModelAttribute cuando queremos pasar a la vista algo comun a todos los metodos del controlador/ o que queremos tener en todas las vistas: /index /listar etc....
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Paco","Gonzalez","paco@mail.com"), new Usuario("Poco","Campos","poco@mail.com"), new Usuario("Xing","Martinez","xing@mail.com"));
+		return usuarios;
+	}
+	
+	
+	
+}
